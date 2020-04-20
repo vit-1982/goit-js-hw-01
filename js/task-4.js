293d147c0546e4d8536e319297e2df2f8347daa4
unknown <vzntst@gmail.com>
2020-04-20 23:00:50 +0300

    task-4 improved

--- js/task-4.js
+++ js/task-4.js
@@ -1,24 +1,50 @@
 let credits = 23580;
 const pricePerDroid = 3000;
 
-const numberOfDroids = prompt(
+let numberOfDroids = prompt(
   'Введите количество дроидов, которое Вы хотите купить:',
 );
 
 if (numberOfDroids === null) {
   console.log('Отменено пользователем!');
 } else {
-  let totalPrice = Number(numberOfDroids) * pricePerDroid;
-  if (totalPrice > credits) {
-    console.log('Недостаточно средств на счету!');
+  numberOfDroids = Number(numberOfDroids);
+  if (Number.isNaN(numberOfDroids) || numberOfDroids === 0) {
+    console.log('Неверное значение!');
   } else {
-    credits -= totalPrice;
-    if (Number.isNaN(credits) || Number(numberOfDroids) === 0) {
-      console.log('Неверное значение!');
+    let totalPrice = numberOfDroids * pricePerDroid;
+    if (totalPrice > credits) {
+      console.log('Недостаточно средств на счету!');
     } else {
+      credits -= totalPrice;
       console.log(
         `Вы купили ${numberOfDroids} дроидов, на счету осталось ${credits} кредитов.`,
       );
     }
   }
 }
+
+// let credits = 23580;
+// const pricePerDroid = 3000;
+
+// const numberOfDroids = prompt(
+//   'Введите количество дроидов, которое Вы хотите купить:',
+// );
+
+// if (numberOfDroids === null) {
+//   console.log('Отменено пользователем!');
+// } else {
+//   let totalPrice = Number(numberOfDroids) * pricePerDroid;
+//   if (totalPrice > credits) {
+//     console.log('Недостаточно средств на счету!');
+//   } else {
+//     credits -= totalPrice;
+//     if (Number.isNaN(credits) || Number(numberOfDroids) === 0) {
+//       console.log('Неверное значение!');
+//     } else {
+//       console.log(
+//         `Вы купили ${numberOfDroids} дроидов, на счету осталось ${credits} кредитов.`,
+//       );
+//     }
+//   }
+// }
